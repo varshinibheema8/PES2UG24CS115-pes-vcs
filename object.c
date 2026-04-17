f61b03eda0bf569017d34dbcebc48b9a5c0337be
PES2UG24CS115-pes-vcs <varshini.bheema8@gmail.com>
2026-04-17 20:20:00 +0530

    add comment explaining SHA-256 hashing logic

--- object.c
+++ object.c
@@ -8,6 +8,8 @@
 // PROVIDED functions: compute_hash, object_path, object_exists, hash_to_hex, hex_to_hash
 // TODO functions:     object_write, object_read
 
+// Phase 1: SHA-256 hashing implemented
+
 #include "pes.h"
 #include <stdio.h>
 #include <stdlib.h>
